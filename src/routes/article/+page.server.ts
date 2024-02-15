350d0fc7dc619e44012dc92e20d932810045c0b4
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-15 00:17:05 -0500

    feat: :construction: add absolute path to img + pass UA to websites

--- src/routes/article/+page.server.ts
+++ src/routes/article/+page.server.ts
@@ -1,29 +1,51 @@
-// src/routes/blog/index.server.ts
 import { Readability } from '@mozilla/readability';
-import axios, { AxiosError } from 'axios';
-import jsdom from 'jsdom';
+import axios, { isAxiosError } from 'axios';
+import { JSDOM } from 'jsdom';
+import { URL } from 'url'; // Import URL module for working with URLs
 
-export async function load() {
-	const url = 'https://antithesis.com/blog/is_something_bugging_you/';
+export async function load({ request }) {
+	const headers = request.headers;
+	const userAgent = headers.get('user-agent');
+
+	const url = 'https://lineageos.org/Changelog-28/';
 	const domainMatch = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)\//i);
 
 	try {
-		const { JSDOM } = jsdom;
-		const page = await axios.get(url); // Fetching from the test URL
+		const page = await axios.get(url, {
+			headers: {
+				'User-Agent': userAgent // Set the user agent to prevent blocking
+			}
+		}); // Fetching from the test URL
 
-		const doc = new JSDOM(page.data);
-		const article = new Readability(doc.window.document).parse();
+		const dom = new JSDOM(page.data);
+		const document = dom.window.document;
+
+		// Modify image paths to absolute URLs
+		const images = document.querySelectorAll('img');
+		images.forEach((imgElement) => {
+			const imgSrc = imgElement.getAttribute('src');
+			if (imgSrc && !imgSrc.startsWith('http')) {
+				// Convert relative image path to absolute URL
+				const absoluteUrl = new URL(imgSrc, url).href;
+				imgElement.setAttribute('src', absoluteUrl);
+			}
+		});
+
+		// Parse the modified HTML using Readability
+		const article = new Readability(document).parse();
 
 		return {
 			post: {
 				...article,
-				url: domainMatch?.[1] // Extract domain only
+				url,
+				domain: domainMatch?.[1]
 			}
 		};
 	} catch (error) {
+		console.error(error);
 		return {
-			status: (error as AxiosError).response?.status || 500,
-			error: new Error('Error fetching page data')
+			status: isAxiosError(error) ? error.response?.status : 500,
+			error: isAxiosError(error) ? error.response?.data : error
 		};
 	}
 }
