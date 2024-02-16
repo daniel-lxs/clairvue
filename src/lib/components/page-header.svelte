16024137ebb97ab6d79b2fb23fd743c7042fd473
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-16 17:31:28 -0500

    feat: create page header component

---
+++ src/lib/components/page-header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Separator } from '@/components/ui/separator';
+
+	export let title: string;
+	export let description: string | undefined = undefined;
+</script>
+
+<div class="space-y-2">
+	<h1 class="text-3xl font-bold">{title}</h1>
+	{#if description}
+		<p class="text-md text-muted-foreground">{description}</p>
+	{/if}
+</div>
+<Separator class="my-6" />
