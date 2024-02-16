3899dd712a11d6ac4a7d6ba3dd8baf4feaf4c0c9
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-16 17:29:51 -0500

    feat: :sparkles: create article card component

---
+++ src/lib/components/article-card.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import * as Card from '@/components/ui/card';
+	import type { Article } from '@/data/schema';
+	import { calculateAge } from '@/utils';
+	import { badgeVariants } from '@/components/ui/badge';
+
+	export let article: Article;
+	export let RSSFeedName: string;
+
+	let age = calculateAge(article.pubDate);
+
+	const domainMatch = article.link.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)\//i);
+	let domain = domainMatch?.[1];
+</script>
+
+<Card.Root class="transition-colors hover:bg-muted ">
+	<Card.Header class="pb-2 pt-6">
+		<Card.Title tag="h1" class="text-lg font-bold transition-colors hover:text-primary">
+			<a href="/article/{article.id}" target="_blank">{article.title}</a>
+		</Card.Title>
+		<Card.Description class="text-sm">
+			<a href={article.link} target="_blank" class="hover:text-primary">({domain})</a>
+		</Card.Description>
+	</Card.Header>
+	<Card.Content class="py-2">
+		<p>
+			{article.excerpt}
+		</p>
+	</Card.Content>
+	<Card.Footer>
+		<span class="text-sm text-muted-foreground">{age} ago</span>
+		<div class="flex-grow"></div>
+		<a
+			href="/dashboard"
+			class="{badgeVariants({
+				variant: 'secondary'
+			})} hover: bg-muted-foreground text-popover transition-colors hover:text-primary"
+			>{RSSFeedName}</a
+		>
+	</Card.Footer>
+</Card.Root>
