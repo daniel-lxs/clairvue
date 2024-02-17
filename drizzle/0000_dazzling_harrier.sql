2414118fd91c132696f42b6af96e1673fba1d64c
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-17 18:53:52 -0500

    chore: :card_file_box: regenerate migrations

--- drizzle/0000_dazzling_harrier.sql
+++ drizzle/0000_dazzling_harrier.sql
@@ -2,14 +2,15 @@ CREATE TABLE `articles` (
 	`id` text PRIMARY KEY NOT NULL,
 	`title` text NOT NULL,
 	`link` text NOT NULL,
+	`excerpt` text NOT NULL,
 	`pubDate` integer,
 	`rssFeedId` text,
 	`createdAt` integer NOT NULL,
 	FOREIGN KEY (`rssFeedId`) REFERENCES `rssFeeds`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-CREATE TABLE `feeds` (
-	`id` text PRIMARY KEY NOT NULL,
+CREATE TABLE `boards` (
+	`id` integer PRIMARY KEY NOT NULL,
 	`slug` text NOT NULL,
 	`name` text NOT NULL,
 	`editCode` text NOT NULL,
@@ -17,18 +18,20 @@ CREATE TABLE `feeds` (
 	`updatedAt` integer NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE `rssFeedToFeed` (
+	`rssFeedId` integer NOT NULL,
+	`boardId` integer NOT NULL,
+	FOREIGN KEY (`rssFeedId`) REFERENCES `rssFeeds`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`boardId`) REFERENCES `boards`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
 CREATE TABLE `rssFeeds` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` integer PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
-	`description` text,
+	`description` text DEFAULT 'No description' NOT NULL,
 	`link` text NOT NULL,
 	`createdAt` integer NOT NULL,
 	`updatedAt` integer NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE `rssFeedToFeed` (
-	`rssFeedId` text NOT NULL,
-	`feedId` text NOT NULL,
-	FOREIGN KEY (`rssFeedId`) REFERENCES `rssFeeds`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`feedId`) REFERENCES `feeds`(`id`) ON UPDATE no action ON DELETE no action
-);
+CREATE UNIQUE INDEX `rssFeeds_link_unique` ON `rssFeeds` (`link`);
