ceddfdcf2ea7a85a0a0028ae69a5af05c8a59305
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-16 19:33:54 -0500

    feat: :sparkles: create edit feed dialog component

---
+++ src/lib/components/edit-feed-dialog.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Button, buttonVariants } from '@/components/ui/button';
+	import * as Dialog from '@/components/ui/dialog';
+	import { Input } from '@/components/ui/input';
+	import { Label } from '@/components/ui/label';
+</script>
+
+<Dialog.Root>
+	<Dialog.Trigger class={buttonVariants({ variant: 'default' })}>Edit</Dialog.Trigger>
+
+	<Dialog.Content class="sm:max-w-[425px]">
+		<Dialog.Header>
+			<Dialog.Title>Edit RSS feed</Dialog.Title>
+			<Dialog.Description>Edit the link of the RSS feed. Click save when you're done</Dialog.Description>
+		</Dialog.Header>
+		<div class="grid gap-4 py-4">
+			<div class="grid grid-cols-4 items-center gap-4">
+				<Label class="text-right">Link</Label>
+				<Input id="name" value="https://example.com/feed1" class="col-span-3" />
+			</div>
+			<p class="text-center text-xs text-muted-foreground">
+				The name and description will be fetched automatically
+			</p>
+		</div>
+		<Dialog.Footer>
+			<Button type="submit">Save changes</Button>
+		</Dialog.Footer>
+	</Dialog.Content>
+</Dialog.Root>
