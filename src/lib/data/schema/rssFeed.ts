9c4adc30432fcc22842f9b33f5ad4b8302145d8d
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-17 19:02:25 -0500

    refactor: :recycle: change links to be unique

--- src/lib/data/schema/rssFeed.ts
+++ src/lib/data/schema/rssFeed.ts
@@ -5,16 +5,16 @@ export const rssFeedSchema = sqliteTable('rssFeeds', {
 	id: text('id').primaryKey().notNull(),
 	name: text('name').notNull(),
 	description: text('description').notNull().default('No description'),
-	link: text('link').notNull(),
+	link: text('link').unique().notNull(),
 	createdAt: integer('createdAt', { mode: 'timestamp' }).notNull(),
 	updatedAt: integer('updatedAt', { mode: 'timestamp' }).notNull()
 });
 
-export type RSSFeed = {
+export type rssFeed = {
 	id: string;
 	name: string;
-	description: string;
 	link: string;
+	description?: string;
 	articles?: Article[];
 	createdAt: Date;
 	updatedAt: Date;
