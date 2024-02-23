2f8f26f7a9380371f0b919e1c52ca0aad0819cb8
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-23 00:15:38 -0500

    chore: :card_file_box: generate db migrations

---
+++ drizzle/0000_rare_boomer.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS "articles" (
+	"id" varchar(8) PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"link" text NOT NULL,
+	"excerpt" text NOT NULL,
+	"rssFeedId" text NOT NULL,
+	"publishedAt" timestamp DEFAULT now(),
+	"createdAt" timestamp DEFAULT now(),
+	"updatedAt" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "boards" (
+	"id" varchar(8) PRIMARY KEY NOT NULL,
+	"slug" text NOT NULL,
+	"name" text NOT NULL,
+	"editCode" text NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "boardsToRssFeeds" (
+	"boardId" varchar(8) NOT NULL,
+	"rssFeedId" varchar(8) NOT NULL,
+	CONSTRAINT "boardsToRssFeeds_boardId_rssFeedId_pk" PRIMARY KEY("boardId","rssFeedId")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "rssFeeds" (
+	"id" varchar(8) PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"description" text DEFAULT 'No description' NOT NULL,
+	"link" text NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "rssFeeds_link_unique" UNIQUE("link")
+);
