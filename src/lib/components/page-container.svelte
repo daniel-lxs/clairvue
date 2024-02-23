3a5db0da42d1b28a820d9fe810e61ce68b0ee6f9
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-23 00:26:16 -0500

    feat: :sparkles: add footer to page container

--- src/lib/components/page-container.svelte
+++ src/lib/components/page-container.svelte
@@ -1,3 +1,10 @@
-<div class="container mx-auto max-w-screen-lg pb-10 pt-20">
+<div
+	class="container relative mx-auto flex min-h-[calc(100vh-4rem)] max-w-screen-lg flex-col pb-10 pt-16"
+>
 	<slot />
+
+	<div class="flex-grow"></div>
+	<div class="footer mt-4 w-full pb-8">
+		<slot name="footer" />
+	</div>
 </div>
