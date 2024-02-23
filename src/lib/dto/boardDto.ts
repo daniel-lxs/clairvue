1065b6a381c002dc0d09cb3d1e0d79ff5bad7973
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-23 00:33:45 -0500

    fix: :bug: remove incorrect field from dto

--- src/lib/dto/boardDto.ts
+++ src/lib/dto/boardDto.ts
@@ -1,9 +1,7 @@
 import { z } from 'zod';
 
 export const createBoardDto = z.object({
-	slug: z.string().min(4).max(255),
-	name: z.string().min(4).max(255),
-	editCode: z.string().min(8).max(255)
+	name: z.string().min(4).max(255)
 });
 
 export type CreateBoardDto = z.infer<typeof createBoardDto>;
