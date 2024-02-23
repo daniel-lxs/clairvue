26431009227da60c61997f97cdb5ffdab8422db3
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-23 00:28:13 -0500

    feat: :sparkles: show a card on an empty list

--- src/lib/components/rssfeeds-table.svelte
+++ src/lib/components/rssfeeds-table.svelte
@@ -34,41 +34,58 @@
 	}
 </script>
 
-<div>
-	{#if $board?.rssFeeds}
-		<div class="space-y-4">
-			<div class="space-y-4">
-				<div class="space-y-2">
-					<Label for="feedUrls">RSS feeds</Label>
-					<p class="text-sm text-muted-foreground">Add, edit or remove RSS feeds</p>
-				</div>
-				<CreateFeedDialog on:create={saveNewRssFeed} />
-			</div>
-			{#each rssFeeds as rssFeed, i (i)}
-				<div class="hover-trigger">
-					<Card.Root class="relative transition-colors hover:bg-muted">
-						<Card.Header class="pb-2 pt-6">
-							<Card.Title>
-								<a href={rssFeed.link} target="_blank">{rssFeed.name}</a>
-							</Card.Title>
-						</Card.Header>
-						<Card.Content class="py-2">
-							<p>
-								{rssFeed.description}
-							</p>
-						</Card.Content>
-						<Card.Footer>
-							<a href={rssFeed.link} target="_blank" class="hover:text-primary">{rssFeed.link}</a>
-							<div class="flex-grow"></div>
-							<div class="edit-delete-buttons space-x-2">
-								<EditFeedDialog on:edit={editRssFeed} feed={rssFeed} />
-								<Button class="bg-destructive">Delete</Button>
-							</div>
-						</Card.Footer>
-					</Card.Root>
-				</div>
-			{/each}
+<div class="space-y-4">
+	<div class="space-y-4">
+		<div class="space-y-2">
+			<Label for="feedUrls">RSS feeds</Label>
+			<p class="text-sm text-muted-foreground">Add, edit or remove RSS feeds</p>
 		</div>
+		{#if $board?.rssFeeds.length > 0}
+			<CreateFeedDialog on:create={saveNewRssFeed} />
+		{/if}
+	</div>
+	{#if $board?.rssFeeds.length > 0}
+		{#each rssFeeds as rssFeed, i (i)}
+			<div class="hover-trigger">
+				<Card.Root class="relative transition-colors hover:bg-muted">
+					<Card.Header class="pb-2 pt-6">
+						<Card.Title>
+							<a href={rssFeed.link} target="_blank">{rssFeed.name}</a>
+						</Card.Title>
+					</Card.Header>
+					<Card.Content class="py-2">
+						<p>
+							{rssFeed.description}
+						</p>
+					</Card.Content>
+					<Card.Footer>
+						<a href={rssFeed.link} target="_blank" class="hover:text-primary">{rssFeed.link}</a>
+						<div class="flex-grow"></div>
+						<div class="edit-delete-buttons space-x-2">
+							<EditFeedDialog on:edit={editRssFeed} feed={rssFeed} />
+							<Button class="bg-destructive">Delete</Button>
+						</div>
+					</Card.Footer>
+				</Card.Root>
+			</div>
+		{/each}
+	{:else}
+		<Card.Root class="flex flex-col items-center space-y-12 p-4">
+			<Card.Header class="pb-2 pt-4">
+				<Card.Title>
+					<h2 class="text-xl font-bold">No RSS Feeds Found</h2>
+				</Card.Title>
+			</Card.Header>
+			<Card.Content class="space-y-2 py-2 text-center">
+				<p class="text-muted-foreground">
+					It looks like this board doesn't have any RSS feeds added yet.
+				</p>
+				<p class="text-muted-foreground">You can start by adding a new RSS feed.</p>
+			</Card.Content>
+			<Card.Footer>
+				<CreateFeedDialog on:create={saveNewRssFeed} />
+			</Card.Footer>
+		</Card.Root>
 	{/if}
 </div>
 
