acf541f9e6222c48c143ea085fed0817998f360e
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-25 19:05:32 -0500

    feat: :construction: implement scheduler to fetch articles

---
+++ src/scheduler/index.ts
@@ -0,0 +1,30 @@
+import { CronJob } from 'cron';
+import { syncArticlesSequential } from '../lib/data/services/article';
+import rssFeedRepository from '../lib/data/repositories/rssFeed';
+import type { RssFeed } from '../lib/data/schema';
+
+console.log(`[Scheduler] Started at ${new Date().toLocaleString()}`);
+const job = new CronJob('*/5 * * * *', async () => {
+	// Every 5 minutes
+	console.log(`[Scheduler] Running job at ${new Date().toLocaleString()}`);
+
+	let page = 1;
+	const pageSize = 20;
+
+	let rssFeeds: RssFeed[] | undefined;
+	do {
+		rssFeeds = await rssFeedRepository.findAll(pageSize, (page - 1) * pageSize);
+
+		if (rssFeeds.length > 0) {
+			console.log(`[Scheduler] Syncing ${rssFeeds.length} RSS feeds...`);
+			await Promise.all(
+				rssFeeds.map(async (rssFeed) => {
+					await syncArticlesSequential(rssFeed);
+				})
+			);
+		}
+		page++;
+	} while (rssFeeds && rssFeeds.length > 0);
+});
+
+job.start();
