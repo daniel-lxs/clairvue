d583d01a05cbea12d922606ca48412d502158ee6
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-27 15:28:06 -0500

    chore: :fire: remove test file

--- test.ts
+++
@@ -1,8 +0,0 @@
-import Parser from 'rss-parser';
-
-const parser: Parser = new Parser();
-
-(async () => {
-	const feed = await parser.parseURL('https://www.reddit.com/.rss');
-	console.log(feed);
-})();
