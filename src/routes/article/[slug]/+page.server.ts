11fd88626528864ddb6e6e039efbbcfdd652c930
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-28 16:25:28 -0500

    refactor: :recycle: use parsing function from service

--- src/routes/article/[slug]/+page.server.ts
+++ src/routes/article/[slug]/+page.server.ts
@@ -1,73 +1,38 @@
-import { Readability, isProbablyReaderable } from '@mozilla/readability';
-import axios from 'axios';
-import { JSDOM } from 'jsdom';
-import { URL } from 'url';
-import he from 'he';
 import articleRepository from '$lib/data/repositories/article';
 import { redirect } from '@sveltejs/kit';
+import { parseReadableArticle } from '../../../lib/data/services/article.js';
 
 export async function load({ request, params }) {
-	const articleId = params.slug;
-	const headers = request.headers;
-	const userAgent = headers.get('user-agent');
+	const slug = params.slug;
+	const requestHeaders = request.headers;
+	const userAgent = requestHeaders.get('user-agent');
 
-	const article = await articleRepository.findById(articleId);
+	const article = await articleRepository.findById(slug);
 
 	if (!article) {
 		return {
 			status: 404,
 			article: undefined,
+			parsedArticle: undefined,
 			error: 'Article not found'
 		};
 	}
 
 	const domainMatch = article.link.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)\//i);
 
-	let document: Document;
-	try {
-		const page = await axios.get<string>(article.link, {
-			headers: {
-				'User-Agent': userAgent // Set the user agent to prevent blocking
-			},
-			responseType: 'text'
-		}); // Fetching from the test URL
+	const readableArticle = await parseReadableArticle(article.link, userAgent);
 
-		const decodedPage = he.decode(page.data);
-
-		const dom = new JSDOM(decodedPage, {
-			url: article.link
-		});
-		document = dom.window.document;
-
-		// Modify image paths to absolute URLs
-		const images = document.querySelectorAll('img');
-		images.forEach((imgElement) => {
-			const imgSrc = imgElement.getAttribute('src');
-			if (imgSrc && !imgSrc.startsWith('http')) {
-				// Convert relative image path to absolute URL
-				const absoluteUrl = new URL(imgSrc, article.link).href;
-				imgElement.setAttribute('src', absoluteUrl);
-			}
-		});
-	} catch (error) {
-		console.error(`Error occurred while fetching article: ${error}`);
-		redirect(302, article.link); //TODO: temporary solution
+	if (!readableArticle) {
+		redirect(302, article.link);
 	}
 
-	//TODO: play with the options of Readability
-	if (isProbablyReaderable(document)) {
-		// Parse the modified HTML using Readability
-		const readableContent = new Readability(document).parse();
-
-		return {
-			status: 200,
-			article: {
-				...readableContent,
-				link: article.link,
-				domain: domainMatch?.[1]
-			},
-			error: undefined
-		};
-	}
-	redirect(302, article.link);
+	return {
+		status: 200,
+		parsedArticle: readableArticle,
+		article: {
+			...article,
+			domain: domainMatch?.[1]
+		},
+		error: undefined
+	};
 }
