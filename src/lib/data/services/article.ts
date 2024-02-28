7e05245d22473de79c10c13bd18a71266f26276c
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-28 16:09:23 -0500

    feat: clean html before parsing it + allow redirection to same domain

--- src/lib/data/services/article.ts
+++ src/lib/data/services/article.ts
@@ -1,9 +1,15 @@
+import type { NewArticle } from '@/types/NewArticle';
+import { isProbablyReaderable, Readability } from '@mozilla/readability';
+import axios from 'axios';
+import DOMPurify from 'isomorphic-dompurify';
+import he from 'he';
+import { JSDOM } from 'jsdom';
+import { getLinkPreview } from 'link-preview-js';
 import Parser from 'rss-parser';
 import articleRepository from '../repositories/article';
-import rssFeedRepository from '../repositories/rssFeed';
+import rssFeedRepository from '@/data/repositories/rssFeed';
 import type { RssFeed } from '../schema';
-import { getLinkPreview } from 'link-preview-js';
-import type { NewArticle } from '@/types/NewArticle';
+import type { ParsedArticle } from '../../types/ParsedArticle';
 
 export async function fetchRssFeedArticles(link: string) {
 	try {
@@ -39,6 +45,11 @@ export async function syncArticles(rssFeed: RssFeed, parallel: boolean) {
 
 		const newArticles = await processArticles(rssFeed, orderedArticles, parallel);
 
+		if (!newArticles || newArticles.length === 0) {
+			console.log('[Sync] No new articles found.');
+			return;
+		}
+
 		const createdArticles = await createArticles(newArticles);
 
 		if (!createdArticles) {
@@ -60,53 +71,81 @@ async function processArticles(
 	articles: Parser.Item[],
 	parallel: boolean
 ): Promise<NewArticle[]> {
-	const newArticles: NewArticle[] = [];
+	try {
+		const newArticles: NewArticle[] = [];
 
-	const processArticle = async (article: Parser.Item) => {
-		if (!article.link) return;
+		const processArticle = async (article: Parser.Item) => {
+			try {
+				if (!article.link) return;
 
-		const articleExists = await articleRepository.findByLink(article.link);
-		if (articleExists) return;
+				const articleExists = await articleRepository.findByLink(article.link);
+				if (articleExists) return;
 
-		const linkPreview = await getLinkPreview(article.link, {
-			timeout: 2000,
-			headers: {
-				'User-Agent':
-					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+				const linkPreview = await getLinkPreview(article.link, {
+					timeout: 2000,
+					headers: {
+						'User-Agent':
+							'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+					},
+					followRedirects: `manual`,
+					handleRedirects: (baseURL: string, forwardedURL: string) => {
+						const urlObj = new URL(baseURL);
+						const forwardedURLObj = new URL(forwardedURL);
+						// Check if the forwarded URL is within the same domain
+						if (
+							forwardedURLObj.hostname === urlObj.hostname ||
+							forwardedURLObj.hostname === 'www.' + urlObj.hostname ||
+							'www.' + forwardedURLObj.hostname === urlObj.hostname
+						) {
+							return true;
+						} else {
+							return false;
+						}
+					}
+				});
+
+				if (!linkPreview || !isPreviewTypeHtml(linkPreview)) return;
+
+				if (!linkPreview.siteName) {
+					const domainMatch = article.link.match(
+						/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)\//i
+					);
+					linkPreview.siteName = domainMatch?.[1] || '';
+				}
+
+				const newArticle: NewArticle = {
+					rssFeedId: rssFeed.id,
+					title: article.title || linkPreview.title,
+					link: article.link,
+					description: article.summary || linkPreview.description || '',
+					siteName: linkPreview.siteName,
+					image: linkPreview.images?.[0] || '',
+					publishedAt: new Date(article.pubDate as string)
+				};
+
+				newArticles.push(newArticle);
+			} catch (error) {
+				console.error('Error occurred while processing article:', error);
 			}
-		});
-
-		if (!linkPreview || !isPreviewTypeHtml(linkPreview)) return;
-
-		if (!linkPreview.siteName) {
-			const domainMatch = article.link.match(
-				/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)\//i
-			);
-			linkPreview.siteName = domainMatch?.[1] || '';
-		}
-
-		const newArticle: NewArticle = {
-			rssFeedId: rssFeed.id,
-			title: article.title || linkPreview.title,
-			link: article.link,
-			description: article.summary || linkPreview.description || '',
-			siteName: linkPreview.siteName,
-			image: linkPreview.images?.[0] || '',
-			publishedAt: new Date(article.pubDate as string)
 		};
 
-		newArticles.push(newArticle);
-	};
-
-	if (parallel) {
-		await Promise.all(articles.map(processArticle));
-	} else {
-		for (const article of articles) {
-			await processArticle(article);
+		if (parallel) {
+			//split in chunks of 5 articles and await for each chunk to finish
+			for (let i = 0; i < articles.length; i += 5) {
+				const chunk = articles.slice(i, i + 5);
+				await Promise.all(chunk.map(processArticle));
+			}
+		} else {
+			for (const article of articles) {
+				await processArticle(article);
+			}
 		}
-	}
 
-	return newArticles;
+		return newArticles;
+	} catch (error) {
+		console.error('Error occurred during article processing:', error);
+		return [];
+	}
 }
 
 async function createArticles(newArticles: NewArticle[]): Promise<string[] | undefined> {
@@ -130,4 +169,63 @@ function isPreviewTypeHtml(linkPreview: unknown): linkPreview is {
 		'siteName' in linkPreview &&
 		'title' in linkPreview
 	);
+}
+
+export async function parseReadableArticle(
+	link: string,
+	ua?: string | null
+): Promise<ParsedArticle | undefined> {
+	const userAgent =
+		ua ||
+		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
+
+	let document: Document;
+	try {
+		const pageResponse = await axios.get<string>(link, {
+			headers: {
+				'User-Agent': userAgent
+			},
+			responseType: 'text'
+		}); // Fetching from the test URL
+
+		const html = he.decode(pageResponse.data);
+
+		const sanitizedHtml = html
+			.replace(/<style([\S\s]*?)>([\S\s]*?)<\/style>/gim, '')
+			.replace(/<script([\S\s]*?)>([\S\s]*?)<\/script>/gim, '');
+
+		const cleanHtml = DOMPurify.sanitize(sanitizedHtml);
+
+		const dom = new JSDOM(cleanHtml, { url: link });
+
+		document = dom.window.document;
+
+		// Modify image paths to absolute URLs
+		const images = document.querySelectorAll('img');
+		images.forEach((imgElement) => {
+			const imgSrc = imgElement.getAttribute('src');
+			if (imgSrc && !imgSrc.startsWith('http')) {
+				// Convert relative image path to absolute URL
+				const absoluteUrl = new URL(imgSrc, link).href;
+				imgElement.setAttribute('src', absoluteUrl);
+			}
+		});
+	} catch (error) {
+		console.error(`Error occurred while fetching article: ${error}`);
+		return undefined;
+	}
+
+	//TODO: play with the options of Readability
+	if (isProbablyReaderable(document)) {
+		// Parse the modified HTML using Readability
+		const readableArticle = new Readability(document).parse();
+
+		if (!readableArticle) {
+			console.error('Error occurred while parsing article');
+			return undefined;
+		}
+
+		return readableArticle;
+	}
+	return undefined;
 }
