93fc526612b13fdd2dfa2b9a3e74ae3a1a334631
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-03 19:21:33 -0500

    feat: :sparkles: add auth services

---
+++ src/lib/server/services/auth.ts
@@ -0,0 +1,28 @@
+import { Lucia } from 'lucia';
+import { dev } from '$app/environment';
+import { adapter } from '@/server/data/db';
+
+export const lucia = new Lucia(adapter, {
+	sessionCookie: {
+		attributes: {
+			secure: !dev
+		}
+	},
+	getUserAttributes: (attributes) => {
+		return {
+			// attributes has the type of DatabaseUserAttributes
+			username: attributes.username
+		};
+	}
+});
+
+declare module 'lucia' {
+	interface Register {
+		Lucia: typeof lucia;
+		DatabaseUserAttributes: DatabaseUserAttributes;
+	}
+}
+
+interface DatabaseUserAttributes {
+	username: string;
+}
