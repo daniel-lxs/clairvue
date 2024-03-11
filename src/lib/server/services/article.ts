71ee1c5bad5450cb7c386867625688b249b85613
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-10 23:38:33 -0500

    refactor: :recycle: replace metadata library due to lack of features

--- src/lib/server/services/article.ts
+++ src/lib/server/services/article.ts
@@ -4,13 +4,14 @@ import axios from 'axios';
 import DOMPurify from 'isomorphic-dompurify';
 import he from 'he';
 import { JSDOM } from 'jsdom';
-import { getLinkPreview } from 'link-preview-js';
+import urlMetadata from 'url-metadata';
 import Parser from 'rss-parser';
 import articleRepository from '@/server/data/repositories/article';
 import rssFeedRepository from '@/server/data/repositories/rssFeed';
 import type { RssFeed } from '@/server/data/schema';
 import type { ParsedArticle } from '@/types/ParsedArticle';
 import { z } from 'zod';
+import type { ArticleMetadata } from '@/types/ArticleMetadata';
 
 export async function fetchRssFeedArticles(link: string) {
 	try {
@@ -74,52 +75,44 @@ async function processArticles(
 ): Promise<NewArticle[]> {
 	try {
 		const newArticles: NewArticle[] = [];
-
+		const chunkSize = 10;
 		const processArticle = async (article: Parser.Item) => {
 			try {
-				if (!article.link) return;
+				const { link, title, pubDate } = article;
+				if (!link || !z.string().url().safeParse(link).success) return;
 
-				if (z.string().url().safeParse(article.link).success === false) return;
-
-				const articleExists = await articleRepository.findByLink(article.link);
+				const articleExists = await articleRepository.findByLink(link);
 				if (articleExists) return;
 
-				console.log(`[Sync] Processing article: ${article.link}`);
+				console.log(`[Sync] Processing article: ${link}`);
 
-				const linkPreview = await getLinkPreview(article.link, {
-					timeout: 2000,
-					headers: {
+				const metadata = await urlMetadata(link, {
+					timeout: 10000,
+					requestHeaders: {
 						'User-Agent':
 							'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 					}
+				}).catch((error) => {
+					if (error instanceof Error) {
+						console.log('Error occurred while fetching metadata:', error.message);
+					} else {
+						console.log('Error occurred while fetching metadata');
+					}
+					return undefined;
 				});
 
-				const domainMatch = article.link.match(
-					/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)\//i
-				);
+				const siteName = new URL(link).hostname;
 
-				let siteName = domainMatch?.[1] || '';
-				let title = article.title;
-				let description = article.summary;
-				let image = '';
-
-				if (linkPreview && isPreviewTypeHtml(linkPreview)) {
-					siteName = linkPreview.siteName || siteName;
-					title = linkPreview.title || title;
-					description = linkPreview.description || description;
-					image = linkPreview.images?.[0] || '';
-				}
-
-				if (!title || !siteName) return;
+				const articleMetadata = extractArticleMetadata(metadata, siteName);
 
 				const newArticle: NewArticle = {
 					rssFeedId: rssFeed.id,
-					title: title,
-					link: article.link,
-					description: description || null,
-					siteName: siteName,
-					image: image || null,
-					publishedAt: new Date(article.pubDate as string)
+					title: articleMetadata.title || title || 'Untitled',
+					link,
+					description: articleMetadata.description || null,
+					siteName,
+					image: articleMetadata.image || null,
+					publishedAt: new Date(pubDate as string)
 				};
 
 				newArticles.push(newArticle);
@@ -129,10 +122,13 @@ async function processArticles(
 		};
 
 		if (parallel) {
-			//split in chunks of 5 articles and await for each chunk to finish
-			for (let i = 0; i < articles.length; i += 5) {
-				const chunk = articles.slice(i, i + 5);
+			const chunks = Array.from({ length: Math.ceil(articles.length / chunkSize) }, (_, i) =>
+				articles.slice(i * chunkSize, i * chunkSize + chunkSize)
+			);
+
+			for (const chunk of chunks) {
 				await Promise.all(chunk.map(processArticle));
+				await new Promise((resolve) => setTimeout(resolve, 1000)); // 1 second delay
 			}
 		} else {
 			for (const article of articles) {
@@ -150,24 +146,6 @@ async function processArticles(
 async function createArticles(newArticles: NewArticle[]): Promise<string[] | undefined> {
 	const createdIds = await articleRepository.create(newArticles);
 	return createdIds;
-}
-
-function isPreviewTypeHtml(linkPreview: unknown): linkPreview is {
-	contentType: 'text/html';
-	description: string;
-	images: string;
-	siteName: string;
-	title: string;
-} {
-	return (
-		typeof linkPreview === 'object' &&
-		linkPreview !== null &&
-		'contentType' in linkPreview &&
-		'description' in linkPreview &&
-		'images' in linkPreview &&
-		'siteName' in linkPreview &&
-		'title' in linkPreview
-	);
 }
 
 export async function parseReadableArticle(
@@ -227,4 +205,75 @@ export async function parseReadableArticle(
 		return readableArticle;
 	}
 	return undefined;
+}
+
+function extractArticleMetadata(
+	metadata: urlMetadata.Result | undefined,
+	domain: string
+): ArticleMetadata {
+	if (!metadata) {
+		return {};
+	}
+
+	// Extract title
+	const title =
+		metadata.title ||
+		metadata['og:title'] ||
+		metadata['twitter:title'] ||
+		(metadata.jsonld &&
+			(isNewsArticle(metadata.jsonld)
+				? metadata.jsonld.headline
+				: metadata.jsonld.find(isNewsArticle)?.headline));
+
+	// Extract description
+	const description =
+		metadata.description || metadata['og:description'] || metadata['twitter:description'];
+
+	// Extract image
+	const image: string =
+		metadata.image ||
+		metadata['og:image'] ||
+		metadata['twitter:image'] ||
+		metadata['twitter:image:src'] ||
+		metadata['og:image:secure_url'] ||
+		metadata['twitter:image:src'] ||
+		(metadata.jsonld && metadata.jsonld[1]?.image?.[0]?.url) ||
+		'';
+
+	// Validate image URL
+	let validImageUrl: string | undefined;
+
+	//Check if the string can be separated by comma
+	if (image && image.includes(',')) {
+		const imageArray = image.split(',');
+		validImageUrl = imageArray[0];
+	}
+
+	// Check if image is a relative path and add domain if needed
+	if (image && !image.startsWith('http')) {
+		if (domain) {
+			// Construct absolute URL using domain
+			const absoluteUrl = new URL(image, `https://${domain}`).href;
+			validImageUrl = absoluteUrl;
+		} else {
+			// If domain is not available, treat the image as is
+			validImageUrl = image;
+		}
+	} else {
+		// If image is already an absolute URL, use it as is
+		validImageUrl = image;
+	}
+
+	return { title, description, image: validImageUrl };
+}
+
+// Helper function to check if the given data is a NewsArticle JSON-LD object
+function isNewsArticle(
+	jsonldData: unknown
+): jsonldData is { '@type': 'NewsArticle'; headline: string } {
+	if (typeof jsonldData === 'object' && jsonldData !== null) {
+		const data = jsonldData as { '@type'?: string; headline?: string };
+		return data['@type'] === 'NewsArticle' && typeof data.headline === 'string';
+	}
+	return false;
 }
