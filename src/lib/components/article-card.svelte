ba51e5e048a2b9e158e37001e4781db68d65512c
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-10 23:39:18 -0500

    feat: :sparkles: implement a card for articles with no image

--- src/lib/components/article-card.svelte
+++ src/lib/components/article-card.svelte
@@ -3,70 +3,120 @@
 	import type { Article } from '@/server/data/schema';
 	import { calculateAge } from '@/utils';
 	import { badgeVariants } from '@/components/ui/badge';
+	import { Skeleton } from './ui/skeleton';
 
 	export let article: Article;
 
-	let age = calculateAge(new Date(article.publishedAt));
+	$: imageLoaded = false;
+	$: age = calculateAge(new Date(article.publishedAt));
+	$: domain = new URL(article.link).hostname.replace(/^www\./, '');
 
-	const domainMatch = article.link.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)\//i);
-	let domain = domainMatch?.[1];
+	setTimeout(() => {
+		if (!imageLoaded) {
+			imageLoaded = true;
+		}
+	}, 100);
 </script>
 
-<Card.Root class="flex px-6 py-4 transition-colors hover:bg-muted">
-	{#if article.image}
-		<div class="flex w-1/4 items-center justify-center">
-			<div class="relative h-full max-h-48 min-h-48 w-full max-w-full overflow-hidden rounded-lg">
-				<div class="absolute inset-0 bg-gray-200 blur-sm filter">
+{#if article.image}
+	<Card.Root class="flex px-6 py-4 transition-colors hover:bg-muted">
+		<div class="flex w-full flex-row">
+			<div class="w-1/4 pr-4">
+				<div class="relative h-48 w-full overflow-hidden rounded-lg">
+					{#if !imageLoaded}
+						<Skeleton class="h-full w-full object-cover" />
+					{/if}
 					<a href="/article/{article.id}">
 						<img
 							src={article.image}
 							alt={article.title}
 							loading="lazy"
-							class="h-full w-full object-cover"
+							class="h-full w-full object-cover opacity-0 blur-md transition-opacity {imageLoaded
+								? 'opacity-100'
+								: ''}"
 							style="height: 100%; width: 100%;"
+							on:load={() => {
+								imageLoaded = true;
+							}}
+						/>
+					</a>
+
+					<a href="/article/{article.id}">
+						<img
+							src={article.image}
+							alt={article.title}
+							loading="lazy"
+							class="absolute inset-0 h-full w-full object-contain opacity-0 transition-opacity {imageLoaded
+								? 'opacity-100'
+								: ''}"
+							on:load={() => {
+								imageLoaded = true;
+							}}
 						/>
 					</a>
 				</div>
-				<a href="/article/{article.id}">
-					<img
-						src={article.image}
-						alt={article.title}
-						loading="lazy"
-						class="absolute inset-0 h-full w-full object-contain"
-					/>
-				</a>
+			</div>
+			<div class="flex w-3/4 flex-col justify-between">
+				<div>
+					<Card.Header class="pb-2 pt-2">
+						<Card.Title tag="h1" class="text-lg font-bold transition-colors hover:text-primary">
+							<a href="/article/{article.id}">{article.title}</a>
+						</Card.Title>
+						<Card.Description class="text-sm">
+							<a href={article.link} class="hover:text-primary">({domain})</a>
+						</Card.Description>
+					</Card.Header>
+					<Card.Content class="py-2">
+						{#if article.description}
+							<p>
+								{article.description.length > 150
+									? article.description.substring(0, 150) + '...'
+									: article.description}
+							</p>
+						{/if}
+					</Card.Content>
+				</div>
+				<Card.Footer class="flex items-center justify-between pb-0 pr-0 pt-2">
+					<span class="text-sm text-muted-foreground">{age} ago</span>
+					<a
+						href="/dashboard"
+						class="{badgeVariants({
+							variant: 'default'
+						})} transition-colors hover:bg-muted">{article.rssFeed?.name}</a
+					>
+				</Card.Footer>
 			</div>
 		</div>
-	{:else}
-		<div class="w-1/4 flex-grow rounded-lg bg-muted"></div>
-	{/if}
-	<div class="flex w-3/4 flex-col">
-		<Card.Header class="pb-2 pt-2">
-			<Card.Title tag="h1" class="text-lg font-bold transition-colors hover:text-primary">
-				<a href="/article/{article.id}">{article.title}</a>
-			</Card.Title>
-			<Card.Description class="text-sm">
-				<a href={article.link} class="hover:text-primary">({domain})</a>
-			</Card.Description>
-		</Card.Header>
-		<Card.Content class="flex-grow py-2">
+	</Card.Root>
+{:else}
+	<Card.Root class="flex py-2  transition-colors hover:bg-muted">
+		<div class="flex w-full flex-col">
+			<Card.Header class="pb-2 pt-2">
+				<Card.Title tag="h1" class="text-lg font-bold transition-colors hover:text-primary">
+					<a href="/article/{article.id}">{article.title}</a>
+				</Card.Title>
+				<Card.Description class="text-sm">
+					<a href={article.link} class="hover:text-primary">({domain})</a>
+				</Card.Description>
+			</Card.Header>
 			{#if article.description}
-				<p>
-					{article.description.length > 150
-						? article.description.substring(0, 150) + '...'
-						: article.description}
-				</p>
+				<Card.Content class="py-2">
+					<p>
+						{article.description.length > 150
+							? article.description.substring(0, 150) + '...'
+							: article.description}
+					</p>
+				</Card.Content>
 			{/if}
-		</Card.Content>
-		<Card.Footer class="w-full pb-0 pr-0 pt-2">
-			<span class="text-sm text-muted-foreground">{age} ago</span>
-			<div class="flex-grow"></div>
-			<a
-				href="/dashboard"
-				class="{badgeVariants({
-					variant: 'default'
-				})} transition-colors hover:bg-muted">{article.rssFeed?.name}</a
-			>
-		</Card.Footer>
-	</div>
-</Card.Root>
+			<Card.Footer class="flex items-center justify-between pb-2 pr-6 pt-2">
+				<span class="text-sm text-muted-foreground">{age} ago</span>
+				<a
+					href="/dashboard"
+					class="{badgeVariants({
+						variant: 'default'
+					})} transition-colors hover:bg-muted">{article.rssFeed?.name}</a
+				>
+			</Card.Footer>
+		</div>
+	</Card.Root>
+{/if}
