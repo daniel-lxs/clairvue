cbab73ff8cae06ee132b4d8809813846bd8f7fd6
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-17 23:39:42 -0500

    feat: :construction: implement my boards page

---
+++ src/lib/components/board/board-list-item.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Board } from '@/server/data/schema';
+	import { Button } from '../ui/button';
+	import { calculateAge } from '@/utils';
+
+	export let board: Board;
+
+	let rssFeedsCount = board.rssFeeds?.length || 0;
+
+	let isHovered = false;
+</script>
+
+<div
+	class="border-x transition-colors first:rounded-t-lg first:border-t last:rounded-b-lg last:shadow hover:bg-muted"
+	on:mouseenter={() => (isHovered = true)}
+	on:mouseleave={() => (isHovered = false)}
+	role="button"
+	tabindex="0"
+>
+	<div class="flex items-center justify-between border-b px-4 py-4">
+		<div class="item-body flex flex-col">
+			<div class="item-title text-base-content font-semibold">{board.name}</div>
+			<div class="item-description text-sm text-muted-foreground">
+				Created {calculateAge(board.createdAt, 'long')} ago • {rssFeedsCount} RSS feeds
+			</div>
+		</div>
+		<div class="item-actions">
+			{#if isHovered}
+				<Button href="/board/edit/{board.slug}" size="sm" variant="outline">Edit</Button>
+			{/if}
+		</div>
+	</div>
+</div>
--- src/routes/boards/+page.svelte
+++ src/routes/boards/+page.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
 	import * as Page from '@/components/page';
+	import BoardListItem from '@/components/board/board-list-item.svelte';
 	import type { PageData } from './$types';
+	import { Button } from '@/components/ui/button';
 
 	export let data: PageData;
-
-	console.log(JSON.stringify(data));
 </script>
 
 <Page.Container>
-	<Page.Header title="Boards" />
+	<Page.Header title="Boards" subtitle="Add, edit or remove boards" />
+	<div class="space-y-4">
+		<div class="flex">
+			<Button href="/board/new" variant="outline">Add new board</Button>
+		</div>
 
-	<div>
-		{#if data && data.boards && data.boards.length > 0}
-			{#each data.boards as board}
-				<a href="/board/{board.slug}">{board.name}</a>
-			{/each}
-		{/if}
+		<div>
+			{#if data && data.boards && data.boards.length > 0}
+				{#each data.boards as board}
+					<BoardListItem {board} />
+				{/each}
+			{/if}
+		</div>
 	</div>
 </Page.Container>
