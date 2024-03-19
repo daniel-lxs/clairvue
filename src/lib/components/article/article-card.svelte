98dbd1ae54fd0f1be306421817cf2ec0ae95c2d6
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-18 23:45:17 -0500

    refactor: :sparkles: implement better layout for mobile

--- src/lib/components/article/article-card.svelte
+++ src/lib/components/article/article-card.svelte
@@ -5,16 +5,19 @@
 	import ArticleCardImage from './article-card-image.svelte';
 	import { Skeleton } from '../ui/skeleton';
 	import { BookOpen } from 'lucide-svelte';
+	import { onMount } from 'svelte';
 
 	export let article: Article;
 
 	$: imageLoaded = false;
 	$: imageError = false;
-	$: imageObjectType = 'cover';
+	$: imageType = 'wide';
 	$: age = calculateAge(new Date(article.publishedAt));
 	$: imageWidth = 0;
 	$: imageHeight = 0;
 	$: aspectRatio = 0;
+
+	let isMobile = false;
 
 	let descriptionLength: number = 300;
 
@@ -31,7 +34,7 @@
 			aspectRatio = imageWidth / imageHeight;
 
 			if (aspectRatio <= 1.3 || imageWidth < 300) {
-				imageObjectType = 'contain';
+				imageType = 'square';
 				descriptionLength = 200;
 			}
 
@@ -52,77 +55,83 @@
 			imageError = true;
 		}
 	}, 5000);
+
+	onMount(() => {
+		isMobile = window.innerWidth <= 768;
+	});
 </script>
 
 <div>
 	<Card.Root class="flex shadow-lg">
 		<div class="flex w-full flex-col">
-			{#if article.image && !imageError && imageObjectType === 'cover'}
+			{#if article.image && !imageError && imageType === 'wide'}
 				{#if imageLoaded}
 					<div class="w-full">
-						<ArticleCardImage
-							{article}
-							bind:imageLoaded
-							bind:imageError
-							objectType={imageObjectType}
-						/>
+						<ArticleCardImage {article} bind:imageLoaded bind:imageError type={imageType} />
 					</div>
 				{:else}
 					<Skeleton class="h-48 object-cover" />
 				{/if}
 			{/if}
-			<div class="flex w-full justify-between p-4">
-				<div class="flex w-full flex-col justify-between">
-					<div class="space-y-2">
-						<Card.Header class="p-0">
-							<a
-								href="/dashboard"
-								class="text-xs font-bold transition-colors hover:text-primary sm:text-sm"
-								>{article.rssFeed?.name}</a
-							>
-							<div class="flex flex-col gap-1">
-								<Card.Title
-									tag="h1"
-									class="text-lg font-bold transition-colors hover:text-primary sm:text-xl"
+			<div class="space-y-2 p-4">
+				<div class="flex w-full justify-between">
+					<div class="flex w-full flex-col justify-between">
+						<div class="space-y-2">
+							<Card.Header class="p-0">
+								<a
+									href="/dashboard"
+									class="text-xs font-bold transition-colors hover:text-primary sm:text-sm"
+									>{article.rssFeed?.name}</a
 								>
-									<a href="/article/{article.id}">{article.title}</a>
-								</Card.Title>
-								<Card.Description class="sm:text-md text-sm">
-									<div class="flex items-center">
-										<a href={article.link} class="hover:text-primary">({article.siteName})</a>
-										<span class="text-md ml-1 text-muted-foreground">• {age} ago </span>
-										{#if article.readable}
-											<div class="ml-2" title="readable">
-												<BookOpen class="h-4 w-4" />
-											</div>
-										{/if}
-									</div>
-								</Card.Description>
-							</div>
-						</Card.Header>
-
-						<Card.Content class="p-0">
-							{#if article.description}
-								<p class="text-md">
-									{truncateDescription(article.description, descriptionLength)}
-								</p>
+								<div class="flex flex-col gap-1">
+									<Card.Title
+										tag="h1"
+										class="text-lg font-bold transition-colors hover:text-primary sm:text-xl"
+									>
+										<a href="/article/{article.id}">{article.title}</a>
+									</Card.Title>
+									<Card.Description class="sm:text-md text-sm">
+										<div class="flex items-center">
+											<a href={article.link} class="hover:text-primary">({article.siteName})</a>
+											<span class="text-md ml-1 text-muted-foreground">• {age} ago </span>
+											{#if article.readable}
+												<div class="ml-2" title="readable">
+													<BookOpen class="h-4 w-4" />
+												</div>
+											{/if}
+										</div>
+									</Card.Description>
+								</div>
+							</Card.Header>
+							{#if !isMobile || imageType === 'wide'}
+								<Card.Content class="p-0">
+									{#if article.description}
+										<p class="text-md">
+											{truncateDescription(article.description, descriptionLength)}
+										</p>
+									{/if}
+								</Card.Content>
 							{/if}
-						</Card.Content>
-					</div>
-				</div>
-				{#if article.image && !imageError && imageObjectType === 'contain'}
-					{#if imageLoaded}
-						<div class="ml-4 w-1/3 p-2">
-							<ArticleCardImage
-								{article}
-								bind:imageLoaded
-								bind:imageError
-								objectType={imageObjectType}
-							/>
 						</div>
-					{:else}
-						<Skeleton class="h-48 object-cover" />
+					</div>
+					{#if article.image && !imageError && imageType === 'square'}
+						{#if imageLoaded}
+							<div class="ml-4 pl-2">
+								<ArticleCardImage {article} bind:imageLoaded bind:imageError type={imageType} />
+							</div>
+						{:else}
+							<Skeleton class="h-48 object-cover" />
+						{/if}
 					{/if}
+				</div>
+				{#if isMobile && imageType === 'square'}
+					<Card.Content class="p-0">
+						{#if article.description}
+							<p class="text-md">
+								{truncateDescription(article.description, descriptionLength)}
+							</p>
+						{/if}
+					</Card.Content>
 				{/if}
 			</div>
 		</div>
