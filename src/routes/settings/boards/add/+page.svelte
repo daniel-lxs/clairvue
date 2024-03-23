7ea2cca37dd6a9210b3ef1f71fb87f2b2aec248f
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-23 16:51:51 -0500

    feat: :sparkles: add save rss functionality

--- src/routes/settings/boards/add/+page.svelte
+++ src/routes/settings/boards/add/+page.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
-	import type { PageServerData } from './$types';
+	import { createBoard, createRssFeeds } from '@/api';
+	import CreateFeedDialog from '@/components/feed/create-feed-dialog.svelte';
+	import RssFeedListItem from '@/components/feed/rss-feed-list-item.svelte';
 	import * as Page from '@/components/page';
-	import RssfeedsTable from '@/components/feed/rssfeeds-table.svelte';
+	import AlertDialog from '@/components/shared/alert.dialog.svelte';
 	import Button from '@/components/ui/button/button.svelte';
 	import { Input } from '@/components/ui/input';
 	import Label from '@/components/ui/label/label.svelte';
-	import AlertDialog from '@/components/shared/alert.dialog.svelte';
 	import type { Board, RssFeed } from '@/server/data/schema';
-	import { writable } from 'svelte/store';
-	import { createBoard, createRssFeeds } from '@/api';
 	import type { CreateRssFeedDto } from '@/server/dto/rssFeedDto';
+	import type { NewRssFeed } from '@/types/NewRssFeed';
 	import { Loader2 } from 'lucide-svelte';
+	import { writable } from 'svelte/store';
+	import type { PageServerData } from './$types';
+	import { goto } from '$app/navigation';
 
 	export let data: PageServerData;
 
@@ -46,12 +49,11 @@
 				const newRssFeeds: CreateRssFeedDto[] = $board.rssFeeds.map((rssFeed) => ({
 					name: rssFeed.name,
 					description: rssFeed.description,
-					link: rssFeed.link,
-					boardId: newBoard.id
+					link: rssFeed.link
 				}));
 
 				if (newRssFeeds.length > 0) {
-					const createRssFeedResults = await createRssFeeds(newRssFeeds);
+					const createRssFeedResults = await createRssFeeds(newRssFeeds, newBoard.id);
 
 					if (
 						!createRssFeedResults ||
@@ -67,6 +69,7 @@
 				}
 
 				board.set(newBoard);
+				goto('/settings/boards');
 			}
 
 			//TODO: Handle empty board
@@ -83,19 +86,71 @@
 		message = message;
 		open = true;
 	}
+
+	function saveRssFeed(e: CustomEvent<NewRssFeed>) {
+		const newRssFeed = {
+			...e.detail,
+			id: '',
+			createdAt: new Date(),
+			updatedAt: new Date(),
+			syncedAt: new Date(),
+			boardId: $board.id
+		};
+		if ($board && $board.rssFeeds) $board.rssFeeds = [...$board.rssFeeds, newRssFeed];
+
+		$board.rssFeeds?.push(newRssFeed);
+	}
+
+	function removeRssFeed(rssFeed: RssFeed) {
+		if ($board && $board.rssFeeds) {
+			$board.rssFeeds = $board.rssFeeds.filter((f) => f.id !== rssFeed.id);
+		}
+	}
 </script>
 
 <AlertDialog {open} {title} {message} />
 
 <Page.Container>
-	<Page.Header title="New feed" />
+	<Page.Header title="New board" subtitle="Create a new board" />
 
-	<div class="space-y-8">
+	<div class="space-y-12">
 		<div class="space-y-2">
-			<Label>Feed name</Label>
-			<Input type="text" placeholder="Feed name" bind:value={$board.name} />
+			<Label for="feedName" class=" font-semibold">Board name</Label>
+			<Input
+				id="feedName"
+				type="text"
+				placeholder="Feed name"
+				class="w-full"
+				bind:value={$board.name}
+			/>
 		</div>
-		<RssfeedsTable {board} />
+
+		<div>
+			<div class="mb-4 flex items-center justify-between">
+				<div class="space-y-2">
+					<Label for="feedUrls" class=" font-semibold">RSS feeds</Label>
+					<p class="text-sm text-muted-foreground">Add, edit or remove RSS feeds</p>
+				</div>
+
+				<CreateFeedDialog on:create={saveRssFeed} />
+			</div>
+
+			<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+				{#if !$board.rssFeeds || $board.rssFeeds.length === 0}
+					<div class="col-span-full mt-4 flex flex-col items-center justify-center">
+						<h2 class="text-xl font-bold text-foreground">No RSS Feeds Added Yet</h2>
+						<p class="mt-2 text-center text-muted-foreground">
+							Add RSS feeds to your board to get started. You can add multiple feeds to monitor
+							different sources in one place.
+						</p>
+					</div>
+				{:else}
+					{#each $board.rssFeeds as rssFeed}
+						<RssFeedListItem {rssFeed} on:delete={() => removeRssFeed(rssFeed)} />
+					{/each}
+				{/if}
+			</div>
+		</div>
 	</div>
 	<svelte:fragment slot="footer">
 		<Button disabled={isLoading} type="submit" on:click={saveBoard}>
