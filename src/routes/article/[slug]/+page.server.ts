ca8dddc659c62e1426bb43230659b9c1c0e7d72d
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-25 19:44:15 -0500

    refactor: :recycle: convert to arrow function

--- src/routes/article/[slug]/+page.server.ts
+++ src/routes/article/[slug]/+page.server.ts
@@ -1,31 +1,32 @@
 import articleRepository from '@/server/data/repositories/article';
 import { redirect } from '@sveltejs/kit';
 import { parseReadableArticle } from '@/server/services/article';
+import type { PageServerLoad } from './$types';
 
-export async function load({ request, params }) {
-  const slug = params.slug;
-  const requestHeaders = request.headers;
-  const userAgent = requestHeaders.get('user-agent');
+export const load: PageServerLoad = async ({ request, params }) => {
+	const slug = params.slug;
+	const requestHeaders = request.headers;
+	const userAgent = requestHeaders.get('user-agent');
 
-  const article = await articleRepository.findBySlug(slug);
+	const article = await articleRepository.findBySlug(slug);
 
-  if (!article) {
-    return {
-      status: 404,
-      article: undefined,
-      streamed: { parsedArticle: undefined },
-      error: 'Article not found'
-    };
-  }
+	if (!article) {
+		return {
+			status: 404,
+			article: undefined,
+			streamed: { parsedArticle: undefined },
+			error: 'Article not found'
+		};
+	}
 
-  if (!article.readable) {
-    redirect(302, article.link);
-  }
+	if (!article.readable) {
+		redirect(302, article.link);
+	}
 
-  return {
-    status: 200,
-    streamed: { parsedArticle: parseReadableArticle(article.link, userAgent) },
-    article,
-    error: undefined
-  };
-}
+	return {
+		status: 200,
+		streamed: { parsedArticle: parseReadableArticle(article.link, userAgent) },
+		article,
+		error: undefined
+	};
+};
