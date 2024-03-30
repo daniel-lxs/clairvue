601eabfe8ef58a1fae2c80e7601969ba6fc60f8b
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-30 01:19:41 -0500

    revert: :rewind: revert article card changes

--- src/lib/components/article/article-card-image.svelte
+++ src/lib/components/article/article-card-image.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import type { Article } from '@/server/data/schema';
-	import missingImagePlaceholder from '@/assets/missing-img-placeholder.png?enhanced';
-	import { Skeleton } from '../ui/skeleton';
 
 	export let article: Article;
 	export let imageLoaded = false;
@@ -11,23 +9,19 @@
 
 <div class="relative w-full overflow-hidden {type === 'square' ? 'rounded-lg' : 'rounded-t-lg'}">
 	<a href="/article/{article.slug}" target="_blank">
-		{#if imageLoaded || imageError}
-			<enhanced:img
-				src={!imageError && article.image ? article.image : missingImagePlaceholder}
-				alt={article.title}
-				loading="lazy"
-				class="{imageError ? 'h-80' : 'h-full'} w-full bg-muted object-cover {type === 'square'
-					? ' max-w-52'
-					: ''}"
-				on:load={() => {
-					imageLoaded = true;
-				}}
-				on:error={() => {
-					imageError = true;
-				}}
-			/>
-		{:else}
-			<Skeleton class="h-48 object-cover" />
-		{/if}
+		<img
+			src={article.image}
+			alt={article.title}
+			loading="lazy"
+			class="h-full w-full bg-muted object-contain {type === 'square'
+				? 'max-h-36 min-h-24 min-w-24 max-w-52'
+				: ''}"
+			on:load={() => {
+				imageLoaded = true;
+			}}
+			on:error={() => {
+				imageError = true;
+			}}
+		/>
 	</a>
 </div>
