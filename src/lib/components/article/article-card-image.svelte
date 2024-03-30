d4bfccf5806cfe03ebebec3975b2738ffbfffa79
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-29 19:04:31 -0500

    feat: :sparkles: use enhanced image + image placeholder

--- src/lib/components/article/article-card-image.svelte
+++ src/lib/components/article/article-card-image.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import type { Article } from '@/server/data/schema';
+	import missingImagePlaceholder from '@/assets/missing-img-placeholder.png?enhanced';
+	import { Skeleton } from '../ui/skeleton';
 
 	export let article: Article;
 	export let imageLoaded = false;
@@ -9,17 +11,23 @@
 
 <div class="relative w-full overflow-hidden {type === 'square' ? 'rounded-lg' : 'rounded-t-lg'}">
 	<a href="/article/{article.slug}" target="_blank">
-		<img
-			src={article.image}
-			alt={article.title}
-			loading="lazy"
-			class="h-full w-full bg-muted object-contain {type === 'square' ? 'max-h-48 max-w-52' : ''}"
-			on:load={() => {
-				imageLoaded = true;
-			}}
-			on:error={() => {
-				imageError = true;
-			}}
-		/>
+		{#if imageLoaded || imageError}
+			<enhanced:img
+				src={!imageError && article.image ? article.image : missingImagePlaceholder}
+				alt={article.title}
+				loading="lazy"
+				class="{imageError ? 'h-80' : 'h-full'} w-full bg-muted object-cover {type === 'square'
+					? ' max-w-52'
+					: ''}"
+				on:load={() => {
+					imageLoaded = true;
+				}}
+				on:error={() => {
+					imageError = true;
+				}}
+			/>
+		{:else}
+			<Skeleton class="h-48 object-cover" />
+		{/if}
 	</a>
 </div>
