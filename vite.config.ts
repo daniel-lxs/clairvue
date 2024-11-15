d9fa90c25464f14b55809eab2c73c911568307b3
Daniel <ricciodaniel98@gmail.com>
2024-11-15 17:41:35 +0000

    specify server and port

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [sveltekit(), enhancedImages()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
