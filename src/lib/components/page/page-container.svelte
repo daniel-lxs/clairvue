81ad15ef018c06f2b35b6de155013d70b5ef6d22
Daniel Riccio <ricciodaniel98@gmail.com>
2024-11-15 19:31:51 -0500

    improve page container style

--- src/lib/components/page/page-container.svelte
+++ src/lib/components/page/page-container.svelte
@@ -1,10 +1,11 @@
 <div
-  class="container mx-auto flex h-full max-w-screen-2xl flex-col items-center px-4 pb-10 pt-16 sm:max-w-2xl sm:px-0 sm:pt-16"
+  class="container mx-auto min-h-screen flex flex-col items-center px-4 pb-10 pt-16 sm:max-w-2xl sm:px-0 sm:pt-16"
 >
-  <slot />
+  
+    <slot />
+  
 
-  <div class="flex-grow"></div>
-  <div class="footer mt-4 w-full pb-8">
+  <div class="footer mt-auto w-full pb-8">
     <slot name="footer" />
   </div>
 </div>
