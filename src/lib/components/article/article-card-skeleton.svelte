0a13aa92fc4e04cd2317f6af54009df2ef521d6f
Daniel Riccio <ricciodaniel98@gmail.com>
2024-11-17 00:14:14 -0500

    fix article card skeleton structure

--- src/lib/components/article/article-card-skeleton.svelte
+++ src/lib/components/article/article-card-skeleton.svelte
@@ -5,6 +5,7 @@
 
 <Card.Root class="flex w-full shadow-lg">
   <div class="flex w-full flex-col">
+    <Skeleton class="h-48 w-full" />
     <div class="flex w-full justify-between p-4">
       <div class="flex w-full flex-col justify-between">
         <div class="space-y-2">
@@ -12,20 +13,17 @@
             <Skeleton class="h-4 w-32" />
             <div class="mt-2 flex flex-col gap-1">
               <Skeleton class="h-6 w-full" />
-              <Card.Description class="sm:text-md text-sm">
-                <div class="flex items-center">
-                  <Skeleton class="h-4 w-32" />
-                </div>
-              </Card.Description>
             </div>
           </Card.Header>
           <Card.Content class="mt-2 p-0">
+            <div class="mb-2 flex items-center">
+              <Skeleton class="h-4 w-32" />
+            </div>
             <Skeleton class="h-4 w-full" />
             <Skeleton class="mt-2 h-4 w-full" />
           </Card.Content>
         </div>
       </div>
     </div>
-    <Skeleton class="h-48 w-full rounded-t-none" />
   </div>
 </Card.Root>
