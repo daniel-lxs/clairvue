8598295a037aa6df8041f047b5464a761df5c157
Daniel Riccio <ricciodaniel98@gmail.com>
2024-12-06 21:16:13 -0500

    refactor: improve article schema

--- packages/app/src/lib/server/data/schema/article.schema.ts
+++ packages/app/src/lib/server/data/schema/article.schema.ts
@@ -3,19 +3,19 @@ import { feedSchema, type Feed } from './feed.schema';
 import { relations, type InferSelectModel } from 'drizzle-orm';
 
 export const articleSchema = pgTable('articles', {
-  id: serial('id').primaryKey(),
+  id: varchar('id').primaryKey(),
   slug: varchar('slug').notNull(),
   title: text('title').notNull(),
   link: text('link').unique().notNull(),
   feedId: text('feedId').notNull(),
   description: text('description'),
-  siteName: text('siteName'),
+  siteName: text('siteName').notNull(),
   image: text('image'),
   author: text('author'),
   readable: boolean('readable').notNull().default(false),
   publishedAt: timestamp('publishedAt').notNull().defaultNow(),
-  createdAt: timestamp('createdAt').defaultNow(),
-  updatedAt: timestamp('updatedAt').defaultNow()
+  createdAt: timestamp('createdAt').notNull().defaultNow(),
+  updatedAt: timestamp('updatedAt').notNull().defaultNow()
 });
 
 export const articleRelations = relations(articleSchema, ({ one }) => ({
