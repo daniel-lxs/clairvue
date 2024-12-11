e9d62cf8f4e08e68c52dbb0493d91bac3866c9c3
Daniel Riccio <ricciodaniel98@gmail.com>
2024-12-11 18:52:13 -0500

    feat: implement the import article dialog on site header

--- packages/app/src/lib/components/header/site-header.svelte
+++ packages/app/src/lib/components/header/site-header.svelte
@@ -4,12 +4,39 @@
   import { collectionsStore } from '@/stores/collections';
   import { feedsStore } from '@/stores/feeds';
   import Logo from '@/components/ui/logo/logo.svelte';
+  import CreateArticleDialog from '../article/create-article-dialog.svelte';
+  import { createArticle } from '$lib/api/article';
+  import { cn, showToast } from '$lib/utils';
+  import { Result } from '@clairvue/types';
+  import { Import } from 'lucide-svelte';
+  import { DialogTrigger } from '../ui/dialog';
+  import { buttonVariants } from '../ui/button';
+  import { goto } from '$app/navigation';
 
   let collections = $state($collectionsStore);
   let feeds = $state($feedsStore);
 
-  function loadMoreFeeds(page: number) {
-    console.log('load more feeds', page);
+  let defaultFeed = $derived(feeds.find((feed) => feed.link.startsWith('default-feed')));
+
+  async function save(articleData: {
+    title: string;
+    url: string;
+    makeReadable: boolean;
+  }): Promise<Result<string[], Error>> {
+    if (!defaultFeed || !defaultFeed.id) {
+      showToast('Error', 'No default feed found', 'error');
+      return Result.err(new Error('No default feed found'));
+    }
+    return await createArticle(defaultFeed.id, articleData);
+  }
+
+  function handleSave(articleSlugs: string[]) {
+    showToast('Success', 'Article saved', 'success', {
+      label: 'View article',
+      onClick: () => {
+        goto(`/article/${articleSlugs[0]}`);
+      }
+    });
   }
 
   $effect(() => {
@@ -34,10 +61,20 @@
     </div>
 
     <div class="flex flex-1 items-center md:hidden">
-      <NavigationMobileSheet {collections} {feeds} onLoadMoreFeeds={loadMoreFeeds} />
+      <NavigationMobileSheet {collections} {feeds} />
     </div>
-
     <div class="flex items-center space-x-2 md:ml-auto">
+      <CreateArticleDialog showButton={true} {save} onSave={handleSave}>
+        <DialogTrigger
+          title="Import article"
+          class={cn(
+            buttonVariants({ variant: 'ghost', size: 'icon' }),
+            'bg-background/95 supports-[backdrop-filter]:bg-background/60 h-8 w-8 backdrop-blur sm:h-10 sm:w-10'
+          )}
+        >
+          <Import class="h-4 w-4" />
+        </DialogTrigger>
+      </CreateArticleDialog>
       <AppearancePopover />
     </div>
   </div>
