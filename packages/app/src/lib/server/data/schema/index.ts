dd58948b21b5d5986ef504c4230c40f813045157
Daniel Riccio <ricciodaniel98@gmail.com>
2024-12-13 18:09:45 -0500

    feat: export new schema

--- packages/app/src/lib/server/data/schema/index.ts
+++ packages/app/src/lib/server/data/schema/index.ts
@@ -2,3 +2,4 @@ export * from './collection.schema';
 export * from './feed.schema';
 export * from './user.schema';
 export * from './article.schema';
+export * from './user-article-interaction.schema';
