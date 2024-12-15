a12e15d60c6066d77baf53078d457403c8637b52
Daniel Riccio <ricciodaniel98@gmail.com>
2024-12-14 21:37:20 -0500

    refactor: streamline authentication and article retrieval logic in collection page server
    - Replaced the authentication session validation method with a more direct approach using locals.
    - Refactored article retrieval to utilize the new article service, enhancing the handling of user interactions (read/saved).
    - Improved error handling for collection and article fetching, ensuring clearer responses for missing data.
    - Cleaned up import statements for better organization and consistency.

--- packages/app/src/routes/c/[slug]/+page.server.ts
+++ packages/app/src/routes/c/[slug]/+page.server.ts
@@ -1,90 +1,64 @@
 import { redirect, error } from '@sveltejs/kit';
-import authService from '@/server/services/auth.service';
 import type { PageServerLoad } from './$types';
 import collectionService from '@/server/services/collection.service';
-import type { ArticleWithFeed, PaginatedList, FeedWithArticles } from '@clairvue/types';
-import feedService from '$lib/server/services/feed.service';
+import type { ArticleWithFeed, PaginatedList } from '@clairvue/types';
+import articleService from '$lib/server/services/article.service';
 
-export const load: PageServerLoad = async ({ params: { slug }, cookies }) => {
-  const authSessionResult = await authService.validateAuthSession(cookies);
+export const load: PageServerLoad = async ({ params: { slug }, locals }) => {
+  const { authSession } = locals;
 
-  return authSessionResult.match({
-    err: (e) => {
-      error(500, e.message);
-    },
-    ok: async (authSession) => {
-      if (!authSession) {
-        redirect(302, '/auth/login');
+  if (!authSession) {
+    redirect(302, '/auth/login');
+  }
+
+  const collectionResult = await collectionService.findBySlugWithFeeds(slug, authSession.user.id);
+
+  if (collectionResult.isErr()) {
+    error(500, collectionResult.unwrapErr().message);
+  }
+
+  const collection = collectionResult.unwrap();
+
+  if (!collection) {
+    error(404, 'Collection not found');
+  }
+
+  const limitPerPage = 20;
+
+  const beforePublishedAt = new Date().toISOString();
+
+  const getArticles = async (): Promise<
+    PaginatedList<
+      ArticleWithFeed & {
+        saved: boolean;
+        read: boolean;
       }
+    >
+  > => {
+    const articlesResult = await articleService.findByCollectionIdWithInteractions(
+      collection.id,
+      authSession.user.id,
+      beforePublishedAt,
+      limitPerPage
+    );
 
-      const collectionResult = await collectionService.findBySlugWithFeeds(
-        slug,
-        authSession.user.id
-      );
-
-      if (collectionResult.isErr()) {
-        error(500, collectionResult.unwrapErr().message);
-      }
-
-      const collection = collectionResult.unwrap();
-
-      if (!collection) {
-        error(404, 'Collection not found');
-      }
-
-      const limitPerPage = 20;
-
-      const beforePublishedAt = new Date();
-
-      const getArticles = async (): Promise<
-        PaginatedList<
-          ArticleWithFeed & {
-            saved: boolean;
-            read: boolean;
-          }
-        >
-      > => {
-        const feedsResult = await feedService.findByCollectionIdWithArticles(
-          collection.id,
-          beforePublishedAt,
-          limitPerPage
-        );
-
-        if (feedsResult.isErr()) {
-          error(500, feedsResult.unwrapErr().message);
-        }
-
-        const feeds: FeedWithArticles[] | false = feedsResult.unwrap();
-
-        if (!feeds) {
-          error(404, 'Feeds not found');
-        }
-
-        const articles: (ArticleWithFeed & {
-          saved: boolean;
-          read: boolean;
-        })[] = feeds.flatMap((feed) => {
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          const { articles: _, ...feedWithoutArticles } = feed;
-
-          return feed.articles.map((article) => ({
-            ...article,
-            feed: feedWithoutArticles
-          }));
-        });
-
-        return {
-          items: articles,
-          totalCount: articles.length
-        };
-      };
-
-      return {
-        collection,
-        streamed: {
-          articles: getArticles()
-        }
-      };
+    if (articlesResult.isErr()) {
+      error(500, articlesResult.unwrapErr().message);
     }
-  });
+
+    const articles = articlesResult.unwrap();
+
+    if (!articles) {
+      error(404, 'Articles not found');
+    }
+
+    return articles;
+  };
+
+  return {
+    collection,
+    streamed: {
+      articles: getArticles()
+    }
+  };
 };
