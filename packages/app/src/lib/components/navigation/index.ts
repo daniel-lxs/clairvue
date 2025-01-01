ca5859f451f6a9bdc9099b806dd3900c6ac4a001
Daniel Riccio <ricciodaniel98@gmail.com>
2025-01-01 18:12:56 -0500

    refactor: remove NavigationFeeds component from navigation structure
    - Deleted the NavigationFeeds component to streamline the navigation structure and eliminate unused code.
    - Updated the navigation index to reflect this change, enhancing overall component organization.

--- packages/app/src/lib/components/navigation/index.ts
+++ packages/app/src/lib/components/navigation/index.ts
@@ -1,4 +1,3 @@
 export { default as NavigationMobileSheet } from './navigation-mobile-sheet.svelte';
 export { default as NavigationCollections } from './navigation-collections.svelte';
-export { default as NavigationFeeds } from './navigation-feeds.svelte';
 export { default as NavigationMenu } from './navigation-menu.svelte';
